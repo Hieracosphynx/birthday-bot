269232d396f50ff6bd061b9d0272f5638f2b8ca2
Hieracosphynx <cortezmiguel05@gmail.com>
2021-09-15 17:16:17 -0700

    Donme

--- src/actions/Birthday.js
+++ src/actions/Birthday.js
@@ -3,8 +3,8 @@ import { MessageEmbed } from 'discord.js'
 const Birthday = (client) => {
   const messageEmbed = new MessageEmbed()
     .setColor('#ff4400')
-    .addFields({ name: 'Happy Birthday', value: '<@840199203414474752>' })
-  const channel = client.channels.cache.get('808525314891841556')
+    .addFields({ name: 'Happy Birthday', value: '<@531147604558348288>' })
+  const channel = client.channels.cache.get('840199203414474752')
   channel.send({ embeds: [messageEmbed] })
 }
 
